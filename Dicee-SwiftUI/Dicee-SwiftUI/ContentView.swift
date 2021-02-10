737ef090408e1bc1fb490a4c8842978caf4cf8d9
markkypanc <rphanchotong@gmail.com>
2021-02-11 00:31:47 +0700

    Add assets

--- Dicee-SwiftUI/Dicee-SwiftUI/ContentView.swift
+++ Dicee-SwiftUI/Dicee-SwiftUI/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea(.all)
+            Text("I am Mark")
+        }
     }
 }
 
