4506bb96c80b7aa01c6c3b7e0772b73b7f238ab4
markkypanc <rphanchotong@gmail.com>
2021-02-11 01:08:02 +0700

    Finishing practice

--- Dicee-SwiftUI/Dicee-SwiftUI/ContentView.swift
+++ Dicee-SwiftUI/Dicee-SwiftUI/ContentView.swift
@@ -13,7 +13,33 @@ struct ContentView: View {
             Image("background")
                 .resizable()
                 .ignoresSafeArea(.all)
-            Text("I am Mark")
+            VStack {
+                // Logo
+                Image("diceeLogo")
+                
+                Spacer()
+                
+                // Two dices
+                HStack {
+                    DiceView(n: 4)
+                    DiceView(n: 1)
+                }
+                    .padding(.all)
+                
+                Spacer()
+                
+                // Button
+                Button(action: {}) {
+                    Text("Roll")
+                        .font(.system(size: 23))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                        .frame(width: 300, height: 50, alignment: .center)
+                }
+                .background(Color(.red))
+                .cornerRadius(17)
+            }
         }
     }
 }
@@ -21,5 +47,17 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceView: View {
+    
+    let n: Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding(.all)
     }
 }
